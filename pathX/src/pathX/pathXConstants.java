283f931b82fbc429e14b8aa6fa69ec2bce00dcb0
Steven Liao <Stvnliao@yahoo.com>
2014-04-04 21:19:25 -0400

    Just started

---
+++ pathX/src/pathX/pathXConstants.java
@@ -0,0 +1,13 @@
+package pathX;
+
+/**
+ * This class stores all the constants used by pathX application. We'll
+ * do this here rather than load them from files because many of these are
+ * derived from each other.
+ *
+ * @author 
+ */
+public class pathXConstants 
+{
+    
+}

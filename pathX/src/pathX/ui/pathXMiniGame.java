f30fd9f5012fec7ba9c7957fbd650f98a9b2bd9a
Steven Liao <Stvnliao@yahoo.com>
2014-04-04 23:21:18 -0400

    Added projects
    MiniGameFramework, Audio Manager, XMLUtilities, and PropertiesManager

--- pathX/src/pathX/ui/pathXMiniGame.java
+++ pathX/src/pathX/ui/pathXMiniGame.java
@@ -1,11 +1,45 @@
 package pathX.ui;
 
+import mini_game.MiniGame;
 /**
  * 
  *
  * @author Steven Liao
  */
-public class pathXMiniGame 
+public class pathXMiniGame extends MiniGame
 {
+    @Override
+    public void initAudioContent()
+    {
+        
+    }
     
+    @Override
+    public void initData()
+    {
+        
+    }
+    
+    @Override
+    public void initGUIControls()
+    {
+        
+    }
+    
+    @Override
+    public void initGUIHandlers()
+    {
+        
+    }
+    
+    @Override
+    public void reset()
+    {
+        
+    }
+    @Override
+    public void updateGUI()
+    {
+        
+    }
 }
